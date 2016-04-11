fd82ae8b9ba428d79e6e8c59db46b692f919740e
Kyle Wei <weikyle@wustl.edu>
2016-04-11 18:24:58 -0500

    js

---
+++ 03_paragraph/scripts.js
@@ -0,0 +1,4 @@
+document.write("<h1>My first JavaScript program</h1>");
+document.write("<p>This is paragraph one</p>");
+document.write("<p>This is paragraph two</p>");
+document.write("<p>This is paragraph three</p>");
