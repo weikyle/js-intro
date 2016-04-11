0c1778160c829948d58640d481ec5c83f94c0e22
weikyle <weikyle@wustl.edu>
2016-04-11 18:11:20 -0500

    Update scripts.js

--- 01_console/scripts.js
+++ 01_console/scripts.js
@@ -1,4 +1,6 @@
+<script>
 alert("Help me fix this program!");
 alert(Can you get this message to appear?);
 document.write("<h2>My first JavaScript program</h2>");
 document.wrong("I'm practicing 'debugging'.");
+</script>
